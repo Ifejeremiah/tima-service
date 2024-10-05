c5f3814a796a1f4f53c0f2c0b1489a3db505445f
Ife Jeremiah <ifedun.jeremiah@gmail.com>
2024-10-05 22:14:01 +0100

    include password in db query

--- src/main/java/com/tima/repository/UserRepository.java
+++ src/main/java/com/tima/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.repository.Query;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    @Query(value = "{email:'?0'}", fields = "{'firstName': 1, 'lastName': 1, 'email': 1, 'status': 1, 'emailConfirmed':  1}")
+    @Query("{email:'?0'}")
     Optional<User> findByEmail(String email);
 
     Boolean existsByEmail(String email);
