f227e3584d13f7398a4c47df5b3fa064088f5a9d
Ife Jeremiah <ifedun.jeremiah@gmail.com>
2025-12-17 20:19:46 +0100

    write test for reading xlsx files

--- src/test/java/com/tima/io/FileReaderTest.java
+++ src/test/java/com/tima/io/FileReaderTest.java
@@ -3,12 +3,19 @@ package com.tima.io;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tima.model.Question;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 
 @Slf4j
 class FileReaderTest {
@@ -25,5 +32,27 @@ class FileReaderTest {
         } catch (Exception error) {
             log.error("Error reading file", error);
         }
+    }
+
+    @Test
+    public void readXlsxFile() {
+        ClassPathResource resource = new ClassPathResource("Book.xlsx");
+        try (InputStream is = resource.getInputStream(); Workbook workbook = new XSSFWorkbook(is)) {
+            Sheet sheet = workbook.getSheetAt(0);
+            Row dataRow = sheet.getRow(1);
+            log.info(dataRow.getCell(0).getStringCellValue());
+            LinkedHashSet<String> options = convertStringArrayToSet(dataRow.getCell(1).getStringCellValue());
+            for (String option : options) {
+                log.info("option: {}", option);
+            }
+            log.info(dataRow.getCell(2).getStringCellValue());
+        } catch (Exception error) {
+            log.error("Error reading xlsx file", error);
+        }
+    }
+
+    private LinkedHashSet<String> convertStringArrayToSet(String fields) {
+        String[] optionList = fields.trim().split("\\|");
+        return new LinkedHashSet<>(Arrays.asList(optionList));
     }
 }
